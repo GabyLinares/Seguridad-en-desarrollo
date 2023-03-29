45b962f5d571d986fe154d804ceda2b7a5995d7e
GabyLinares <60821287+GabyLinares@users.noreply.github.com>
2023-03-29 16:30:48 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 URL_DB='D:/Escritorio/Universidad/Seguridad en el ciclo/Registro-Seguridad-en-el-ciclo/RegistrobaseDatos2'
 NOT_EMPTY_INPUTS = ['username','password','password2','email']
 TBL_NAME='usuarios3'
-DB_HOST = '34.230.74.225:3306'
+DB_HOST = '34.230.74.225'
 DB_USER = 'Konan'
 DB_PASSWORD = '$%cstx7c6dvfghjFTFnh1?'
 DB_NAME = 'RegistrobaseDatos2'
